e33519c0dc5408368d06d1bc66bee691ed2513b2
Christof Musik <christof@senfdax.de>
2008-12-29 00:09:42 +0100

    fixed license header

--- src/readerserver.h
+++ src/readerserver.h
@@ -1,26 +1,26 @@
 /*
   Copyright 2008 Christof Musik <christof at senfdax.de>
 
-  This file is part of osdmixer.
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
 
-  Foobar is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  Foobar is distributed in the hope that it will be useful,
+  This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
 
   You should have received a copy of the GNU General Public License
-  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
-
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 #include <QTcpServer>
 #include "osd.h"
 
 #ifndef  READERSERVER_INC
+#define  READERSERVER_INC
+
 class ReaderServer : public QTcpServer {
 	Q_OBJECT
 
@@ -32,7 +32,6 @@ class ReaderServer : public QTcpServer {
 
 		OSD *gui;
 };
-#define  READERSERVER_INC
 
-#endif   /* ----- #ifndef READERSERVER_INC  ----- */
+#endif
 
